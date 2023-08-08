6572442bf12d428f493b339deef5a41b0358a03f
tsiguenz <tsiguenz@student.42.fr>
2023-08-08 23:30:00 +0200

    add strdup tests

--- test/src/strdup_tests.c
+++ test/src/strdup_tests.c
@@ -3,9 +3,18 @@
 #define fct ft_strdup
 
 char *strdup_tests() {
-  char *str = fct("42");
-  mu_assert("return string of strcmp(str, \" 42 \")", strcmp(str, "42") == 0);
-  errno = 0;
+  char *str = NULL;
+
+	str = fct("42");
+  mu_assert("strcmp ft_strdup(\"42\")", strcmp(str, "42") == 0);
   free(str);
+
+	str = fct("");
+  mu_assert("strcmp ft_strdup(\"\")", strcmp(str, "") == 0);
+  free(str);
+
+  mu_assert("check errno after ft_strdup", errno == 0);
+
+  errno = 0;
   return 0;
 }
